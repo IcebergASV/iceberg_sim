fd1ee01dc40524f27bbdc97b2eb3d142a2b8b6c9
gpearcey <pearcey.grace@gmail.com>
2024-05-04 11:48:03 -0230

    Add world file

--- launch/wildthumper_empty_world.launch.py
+++ launch/wildthumper_empty_world.launch.py
@@ -49,7 +49,7 @@ from launch_ros.substitutions import FindPackageShare
 def generate_launch_description():
     """Generate a launch description for a wildthumper rover."""
     pkg_project_bringup = get_package_share_directory("ardupilot_gz_bringup")
-    pkg_project_gazebo = get_package_share_directory("ardupilot_gz_gazebo")
+    pkg_project_gazebo = get_package_share_directory("iceberg_sim")
     pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
 
     # Wild Thumper rover.
@@ -75,7 +75,7 @@ def generate_launch_description():
         ),
         launch_arguments={
             "gz_args": "-v4 -s -r "
-            f'{Path(pkg_project_gazebo) / "worlds" / "wildthumper_playpen.sdf"}'
+            f'{Path(pkg_project_gazebo) / "worlds" / "wildthumper_empty_world.sdf"}'
         }.items(),
     )
 
